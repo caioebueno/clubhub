e7bd1e0e3313a59eb6bbd4aa1e4b0b7647bda557
caio bueno <caioebueno@gmail.com>
2021-02-02 16:34:36 -0300

    done

--- screens/home/index.jsx
+++ screens/home/index.jsx
@@ -26,134 +26,132 @@ function Home() {
                 height: '100%'
             }}>
                 <Container>
-                    <div style={{
-                        height: 2000
-                    }}>
-                        <Logo />
-                        <div>
-                            <Title
-                                style={minimize ? {
-                                    fontSize: 22,
-                                    marginBottom: 40
-                                } : {}}
-                            >
-                            THE FUTURE FOR <br />
-                    INDEPENDENT {minimize && <br />} CREATORS
-                            </Title>
-                            <SubTitle
-                                style={minimize ? {
-                                    fontSize: 16
-                                } : {}}
-                            >
-                            Create your custom {minimize && <br />} <StyledText>fan club</StyledText> and monetize <br />
-                    your loyal followers
-                            </SubTitle>
-                        </div>
-                        <Row
+                    
+                    <Logo />
+                    <div>
+                        <Title
                             style={minimize ? {
-                                flexDirection: 'column',
-                                height: '45vh'
+                                fontSize: 22,
+                                marginBottom: 40
                             } : {}}
                         >
-                            <Item
+                            THE FUTURE FOR <br />
+                    INDEPENDENT {minimize && <br />} CREATORS
+                        </Title>
+                        <SubTitle
+                            style={minimize ? {
+                                fontSize: 16
+                            } : {}}
+                        >
+                            Create your custom {minimize && <br />} <StyledText>fan club</StyledText> and monetize <br />
+                    your loyal followers
+                        </SubTitle>
+                    </div>
+                    <Row
+                        style={minimize ? {
+                            flexDirection: 'column',
+                            height: '45vh'
+                        } : {}}
+                    >
+                        <Item
+                            style={minimize ? {
+                                flexDirection: 'row'
+                            } : {}}
+                        >
+                            <IconContainer
                                 style={minimize ? {
-                                    flexDirection: 'row'
+                                    height: 50
                                 } : {}}
                             >
-                                <IconContainer
-                                    style={minimize ? {
-                                        height: 50
-                                    } : {}}
-                                >
-                                    <CustomIcon
-                                        small={minimize ? true : false}
-                                    />
-                                </IconContainer>
-                                <ItemText
-                                    style={minimize ? {
-                                        fontSize: 16,
-                                        marginLeft: 10,
-                                        textAlign: 'left'
-                                    } : {}}
-                                >
+                                <CustomIcon
+                                    small={minimize ? true : false}
+                                />
+                            </IconContainer>
+                            <ItemText
+                                style={minimize ? {
+                                    fontSize: 16,
+                                    marginLeft: 10,
+                                    textAlign: 'left'
+                                } : {}}
+                            >
                                 CUSTOM CLUB <br />
                         CURATION
-                                </ItemText>
-                            </Item>
-                            <Item
+                            </ItemText>
+                        </Item>
+                        <Item
+                            style={minimize ? {
+                                flexDirection: 'row'
+                            } : {}}
+                        >
+                            <IconContainer
                                 style={minimize ? {
-                                    flexDirection: 'row'
+                                    height: 50,
                                 } : {}}
                             >
-                                <IconContainer
-                                    style={minimize ? {
-                                        height: 50,
-                                    } : {}}
-                                >
-                                    <img src={UncensoredIcon} style={minimize ? {
-                                        width: 80,
-                                        transform: 'rotate(-30deg)'
-                                    } : {marginTop: 30}}/>
-                                </IconContainer>
-                                <ItemText
-                                    style={minimize ? {
-                                        fontSize: 16,
-                                        marginLeft: -20,
-                                    } : {}}
-                                    blue
-                                >
+                                <img src={UncensoredIcon} style={minimize ? {
+                                    width: 80,
+                                    transform: 'rotate(-30deg)'
+                                } : {marginTop: 30}}/>
+                            </IconContainer>
+                            <ItemText
+                                style={minimize ? {
+                                    fontSize: 16,
+                                    marginLeft: -20,
+                                } : {}}
+                                blue
+                            >
                                 100% UNCENSORED
-                                </ItemText>
-                            </Item>
-                            <Item
+                            </ItemText>
+                        </Item>
+                        <Item
+                            style={minimize ? {
+                                flexDirection: 'row'
+                            } : {}}
+                        >
+                            <IconContainer
                                 style={minimize ? {
-                                    flexDirection: 'row'
+                                    height: 50
                                 } : {}}
                             >
-                                <IconContainer
-                                    style={minimize ? {
-                                        height: 50
-                                    } : {}}
-                                >
-                                    <FanIcon
-                                        small={minimize ? true : false}
-                                    />
-                                </IconContainer>
-                                <ItemText
-                                    style={minimize ? {
-                                        fontSize: 16,
-                                        marginLeft: 10,
-                                    } : {}}
-                                >
+                                <FanIcon
+                                    small={minimize ? true : false}
+                                />
+                            </IconContainer>
+                            <ItemText
+                                style={minimize ? {
+                                    fontSize: 16,
+                                    marginLeft: 10,
+                                } : {}}
+                            >
                                 FAN FIDELITY
-                                </ItemText>
-                            </Item>
-                            <Item
+                            </ItemText>
+                        </Item>
+                        <Item
+                            style={minimize ? {
+                                flexDirection: 'row'
+                            } : {}}
+                        >
+                            <IconContainer
                                 style={minimize ? {
-                                    flexDirection: 'row'
+                                    height: 50
                                 } : {}}
                             >
-                                <IconContainer
-                                    style={minimize ? {
-                                        height: 50
-                                    } : {}}
-                                >
-                                    <MonetizeIcon
-                                        small={minimize ? true : false}
-                                    />
-                                </IconContainer>
-                                <ItemText
-                                    style={minimize ? {
-                                        fontSize: 16,
-                                        marginLeft: 10,
-                                    } : {}}
-                                    blue
-                                >
+                                <MonetizeIcon
+                                    small={minimize ? true : false}
+                                />
+                            </IconContainer>
+                            <ItemText
+                                style={minimize ? {
+                                    fontSize: 16,
+                                    marginLeft: 10,
+                                } : {}}
+                                blue
+                            >
                                 MONETIZE CONTENT
-                                </ItemText>
-                            </Item>
-                        </Row>
-                    </div>
+                            </ItemText>
+                        </Item>
+                    </Row>
+                   
                 </Container>
                 <Contact setMinimize={setMinimize} minimize={minimize} />
             </Breakpoint>
